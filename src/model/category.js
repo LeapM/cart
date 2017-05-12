76b46a87f20d90c2bcfc92f6e784a4c9d451836f
Wang <Luming.Wang@jacobs.com>
2017-05-12 15:24:59 +0800

    add canCheckSee

--- src/model/category.js
+++ src/model/category.js
@@ -1,14 +1,16 @@
-class Category{
-  constructor(data){
-    this.id;
-    this.slug;
-    this.name;
-    this.description;
-    this.parentId;
-    this.ancestor = [];
-  }
+class Category {
+	constructor(data) {
+		this.id;
+		this.slug;
+		this.name;
+		this.description;
+		this.parentId;
+		this.ancestor = [];
+	}
 
-  async static gen(viewer,id){
-    //db.categories.find({_id: {$in: product['category_ids']}})
-  }
+	static async gen(viewer, id) {
+		//db.categories.find({_id: {$in: product['category_ids']}})
+	}
+	static checkCanSee(viewer, data) {
+	}
 }
