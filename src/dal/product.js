1f31f445f3ee031bd6b3bfde2546f8c4e437bef9
Wang <Luming.Wang@jacobs.com>
2017-05-12 16:53:48 +0800

    dal method added

--- src/dal/product.js
+++ src/dal/product.js
@@ -8,5 +8,31 @@
     	} catch (err) {
     		logErr(err);
     	}
-
+    }
+    export async function getOneProduct(query,option) {
+    	try {
+    		const db = await getDB();
+    		const data = await db.products.findOne(query,option);
+    		return data;
+    	} catch (err) {
+    		logErr(err);
+    	}
+    }
+    export async function getProduct(query,option) {
+    	try {
+    		const db = await getDB();
+    		const data = await db.products.find(query,option);
+    		return data;
+    	} catch (err) {
+    		logErr(err);
+    	}
+    }
+    export async function createProduct(data) {
+    	try {
+    		const db = await getDB();
+    		const data = await db.products.insert(data);
+    		return data;
+    	} catch (err) {
+    		logErr(err);
+    	}
     }
